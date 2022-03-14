e1ccf7b0db08356f2f5969ed5633b91bfd3098f4
Dusan Hlavaty <dhlavaty@gmail.com>
2022-03-14 16:32:25 +0100

    Add Node-Red and zigbee2mqtt

---
+++ backup-run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+export ANSIBLE_HOST_KEY_CHECKING=false
+export ANSIBLE_INVENTORY=hosts.yaml
+export ANSIBLE_ASK_PASS=true
+
+# ...now run ansible
+ansible-playbook --tags backup playbook.yaml
